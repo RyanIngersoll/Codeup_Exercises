086a82fa16035f7757923929cdf4b8d2e627e3c9
Ryan Ingersoll <ryan@sridiversified.com>
2014-08-13 14:50:50 -0500

    added next line for output to if_and_or.php

--- if_and_or.php
+++ if_and_or.php
@@ -10,32 +10,32 @@ if (($x < $y) && ($y < $z)) {
 
  if (($x > 0) || ($x < 10)) {
      //if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
-     echo "$x is greater than 0 OR less than 10";
+     echo "$x is greater than 0 OR less than 10\n";
  }
 
  if (($y > 0) || ($y < 10)) {
      //if $y is greater than 0 OR less than 10, echo the result as a sentence "$y is greater than 0 OR less than 10".
-     echo "$y is greater than 0 OR less than 10";
+     echo "$y is greater than 0 OR less than 10\n";
  }
 
  if (($z > 0) || ($z < 10)) {
      //if $z is greater than 0 OR less than 10, echo the result as a sentence "$z is greater than 0 OR less than 10".
-     echo "$z is greater than 0 OR less than 10";
+     echo "$z is greater than 0 OR less than 10\n";
  }
 
  if (($x > 0) && ($x < 10)) {
      //If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
-     echo "$x is greater than 0 and less than 10";
+     echo "$x is greater than 0 and less than 10\n";
  }
 
 if (($y> 0) && ($y < 10)) {
      //If $y is greater than 0 AND less than 10, echo the result as a sentence "$y is greater than 0 AND less than 10".
-     echo "$y is greater than 0 and less than 10";
+     echo "$y is greater than 0 and less than 10\n";
  } 
 
  if (($z> 0) && ($z < 10)) {
      //If $z is greater than 0 AND less than 10, echo the result as a sentence "$z is greater than 0 AND less than 10".
-     echo "$z is greater than 0 and less than 10";
+     echo "$z is greater than 0 and less than 10\n";
  }
 
  
