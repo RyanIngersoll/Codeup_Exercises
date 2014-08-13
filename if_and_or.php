ea47a38f21a5b4b658fc9582e184e3d50cd44f78
Ryan Ingersoll <ryan@sridiversified.com>
2014-08-13 14:46:01 -0500

    commiting and or operators from sublime

---
+++ if_and_or.php
@@ -0,0 +1,44 @@
+<?php
+$x = 0;
+$y = 5;
+$z = 10;
+
+if (($x < $y) && ($y < $z)) {
+     // if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+     echo "{$x} < {$y} < {$z}\n";
+ }
+
+ if (($x > 0) || ($x < 10)) {
+     //if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
+     echo "$x is greater than 0 OR less than 10";
+ }
+
+ if (($y > 0) || ($y < 10)) {
+     //if $y is greater than 0 OR less than 10, echo the result as a sentence "$y is greater than 0 OR less than 10".
+     echo "$y is greater than 0 OR less than 10";
+ }
+
+ if (($z > 0) || ($z < 10)) {
+     //if $z is greater than 0 OR less than 10, echo the result as a sentence "$z is greater than 0 OR less than 10".
+     echo "$z is greater than 0 OR less than 10";
+ }
+
+ if (($x > 0) && ($x < 10)) {
+     //If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
+     echo "$x is greater than 0 and less than 10";
+ }
+
+if (($y> 0) && ($y < 10)) {
+     //If $y is greater than 0 AND less than 10, echo the result as a sentence "$y is greater than 0 AND less than 10".
+     echo "$y is greater than 0 and less than 10";
+ } 
+
+ if (($z> 0) && ($z < 10)) {
+     //If $z is greater than 0 AND less than 10, echo the result as a sentence "$z is greater than 0 AND less than 10".
+     echo "$z is greater than 0 and less than 10";
+ }
+
+ 
+
+
+
