4525c9aa49df4843ff96f3b2137ed247acc8e31e
Ryan Ingersoll <ryan@sridiversified.com>
2014-08-14 16:56:21 -0500

    used mod function inside for loop

---
+++ fizzbuzz.php
@@ -0,0 +1,25 @@
+<?php
+
+/*Write a program that prints the numbers from 1 to 100. But for multiples
+ of three print “Fizz” instead of the number and for the multiples of five print 
+“Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.*/
+
+for ($num = 1; $num<= 100; $num++){
+
+	if (($num % 3 == 0) && ($num % 5 == 0)) {
+		echo "$num - FizzBuzz \n";
+	}
+
+	elseif($num % 5 == 0){
+
+		echo "$num - Buzz \n";
+	}
+	elseif($num % 3 == 0){
+		echo "$num - Fizz \n";
+	}	
+	else {
+	 	echo "$num \n";
+	}
+}
+
+?>
